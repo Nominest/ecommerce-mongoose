544faf614a7b63a15bfd7bf603b9c4d8cff05266
Nominest <nominest3@gmail.com>
2023-03-21 23:02:30 +0800

    mongodb connection

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import cors from "cors";
-import db from "./config/mongooseconfig.js";
-import ProductRouter from "./model/product.js";
+// import db from "./config/mongooseconfig.js";
+import productRouter from "./routes/productApi.js";
 
-const port = 4500;
 const app = express();
+const port = 4500;
+
 app.use(cors());
 app.use(express.json());
-app.use(ProductRouter);
+app.use(productRouter);
+
 // const productRouter = express.Router();
 
 // productRouter.get("/", async (req, res) => {
--- routes/productApi.js
+++ routes/productApi.js
@@ -1,13 +1,51 @@
 import express from "express";
 import Product from "../model/product.js";
-import { Router } from "express";
 import upload from "../util/multerhandler.js";
+import uploadCloud from "../config/cloudinary-config.js";
 
-const ProductRouter = express.Router();
+const productRouter = express.Router();
 
-ProductRouter.post("product/add", upload.single("file"), (req, res) => {
-  console.log(req);
+productRouter.post("/addproduct", upload.single("file"), async (req, res) => {
+  const { secure_url } = await uploadCloud.uploader.upload(req.file.path, {
+    folder: "product",
+  });
+  const newProduct = {
+    image: secure_url,
+    ...JSON.parse(req.body.details),
+  };
+  console.log("NEW PRODUCT:", newProduct);
 });
+
+productRouter.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+    console.log("products get:", products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+export default productRouter;
+// const productRouter = express.Router();
+
+// productRouter.get("/", async (req, res) => {
+//   await Product.find({}).then((res) => {
+//     console.log("products", res);
+//   });
+// });
+
+// db.once("open", () => {
+//   Product.find({}).then((products) => {
+//     console.log("products:", products);
+//   });
+// });
+
+// db.on("error", (error) => {
+//   console.log("error", error);
+// });
+
 // ProductRouter.post("/product/add", async (req, res) => {
 //   const body = req.body;
 //   const newProduct = new Product(body);
@@ -19,5 +57,3 @@ ProductRouter.post("product/add", upload.single("file"), (req, res) => {
 // });
 
 // module.exports = Router;
-
-export default ProductRouter;
